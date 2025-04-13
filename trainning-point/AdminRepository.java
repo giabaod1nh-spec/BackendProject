25eb7afb853551e31dacced78f76297e0e25bc9b
aHyy2699 <huyhoangit26111999@gmail.com>
2025-04-13 17:10:18 +0700

    Add files via upload

---
+++ trainning-point/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.example.trainning.point.repository;
+
+import com.example.trainning.point.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+}
