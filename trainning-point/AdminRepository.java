66921f8b22d8e7cfd5ceea8ae763855e305875a9
aHyy2699 <huyhoangit26111999@gmail.com>
2025-04-13 17:21:52 +0700

    Update AdminRepository.java

--- trainning-point/AdminRepository.java
+++ trainning-point/AdminRepository.java
@@ -1,7 +1,1 @@
-package com.example.trainning.point.repository;
 
-import com.example.trainning.point.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
